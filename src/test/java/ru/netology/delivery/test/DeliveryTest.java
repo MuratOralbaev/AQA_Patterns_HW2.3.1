79b5448a62e7e542ce01037cffe6a3f7f422eaf7
MuratOralbaev <126411720+MuratOralbaev@users.noreply.github.com>
2024-04-05 18:38:48 +0300

    Update DeliveryTest.java

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -21,35 +21,31 @@ class DeliveryTest {
     void setup() {
         open("http://localhost:9999");
     }
-
+    
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-        var daysToAddForSecondMeeting = 5;
+        var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        $("[data-test-id =city] input").setValue(validUser.getCity());
-        $("[data-test-id = date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id = date] input").setValue(firstMeetingDate);
-        $("[data-test-id = name] input").setValue(validUser.getName());
-        $("[data-test-id = phone] input").setValue(validUser.getPhone());
-        $("[data-test-id = agreement]").click();
-        $(byText("Запланировать")).click();
-        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $("[data-test-id ='success-notification'] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + firstMeetingDate))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $("[data-test-id = date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id = date] input").setValue(secondMeetingDate);
-        $(byText("Запланировать")).click();
-        $("[data-test-id = 'replan-notification'] .notification__content")
-                .shouldHave(Condition.exactText("У Вас уже запланирована встреча на другую дату. Перепланировать? "))
-                .shouldBe(Condition.visible);
-        $("[data-test-id = 'replan-notification'] button").click();
-        $("[data-test-id = 'succes-notification']:notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + secondMeetingDate))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='success-notification'] .notification__title").shouldBe(visible).shouldBe(exactText("Успешно!"));
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldBe(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(secondMeetingDate);
+        $(".button").click();
+        $("[data-test-id='replan-notification'] .notification__title").shouldBe(visible).shouldBe(exactText("Необходимо подтверждение"));
+        $("[data-test-id='replan-notification'] .notification__content").shouldBe(visible).shouldBe(exactText("У вас уже запланирована встреча на другую дату. Перепланировать? Перепланировать"));
+        $("[data-test-id='replan-notification'] .button").click();
+        $("[data-test-id='success-notification'] .notification__title").shouldBe(visible).shouldBe(exactText("Успешно!"));
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldBe(exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
